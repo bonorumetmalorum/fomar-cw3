1d4e44c4bfc92cce2e54e83fd30bee07177fe5fb
Govind Venkatesh <venkateshgovind1@gmail.com>
2019-11-05 19:01:11 +0000

    questions up to and including d completed

--- source/main.cpp
+++ source/main.cpp
@@ -51,6 +51,98 @@ void baryinterp(ofstream & image, float xmax, float ymax){
 
 }
 
+void halfplanetest(ofstream & image, float xmax, float ymax){
+    for(int xstep = 0; xstep < xmax; xstep++){
+        for(int ystep = 0; ystep < ymax; ystep++){
+            float alpha = distance(xstep, ystep, 100, 100, 25, 90) / distance(61, 10, 100, 100, 25, 90); //distance from xstep,ystep to CB
+            float beta = distance(xstep, ystep, 61, 10, 25, 90) / distance(100, 100, 61, 10, 25, 90); //distance from xstep,ystep to AC
+            float gamma = distance(xstep, ystep, 100, 100, 61, 10) / distance(25, 90, 100, 100, 61, 10); //distance from xstep,ystep to BA
+            if(alpha < 0){
+                //R
+                image << 255 << " ";
+            }
+            if(alpha >= 0){
+                image << 0 << " ";
+            }
+            if(beta < 0){
+                //G
+                image << 255 << " ";
+            }
+            if(beta >= 0){
+                image << 0 << " ";
+            }
+            if(gamma < 0){
+                //B
+                image << 255 << " " << endl;
+            }
+            if(gamma >= 0){
+                image << 0 << " " << endl;
+            }
+        }
+    }
+
+}
+
+
+void blacktriangle(ofstream & image, float xmax, float ymax){
+    for(int xstep = 0; xstep < xmax; xstep++){
+        for(int ystep = 0; ystep < ymax; ystep++){
+            float alpha = distance(xstep, ystep, 100, 100, 25, 90) / distance(61, 10, 100, 100, 25, 90); //distance from xstep,ystep to CB
+            float beta = distance(xstep, ystep, 61, 10, 25, 90) / distance(100, 100, 61, 10, 25, 90); //distance from xstep,ystep to AC
+            float gamma = distance(xstep, ystep, 100, 100, 61, 10) / distance(25, 90, 100, 100, 61, 10); //distance from xstep,ystep to BA
+            if(alpha < 0){
+                //R
+                image << 255 << " ";
+            }
+            if(alpha >= 0){
+                image << 0 << " ";
+            }
+            if(beta < 0){
+                //G
+                image << 255 << " ";
+            }
+            if(beta >= 0){
+                image << 0 << " ";
+            }
+            if(gamma < 0){
+                //B
+                image << 255 << " " << endl;
+            }
+            if(gamma >= 0){
+                image << 0 << " " << endl;
+            }
+        }
+    }
+
+}
+
+void fullcolour(ofstream & image, float xmax, float ymax){
+    for(int xstep = 0; xstep < xmax; xstep++){
+        for(int ystep = 0; ystep < ymax; ystep++){
+            float alpha = distance(xstep, ystep, 100, 100, 25, 90) / distance(61, 10, 100, 100, 25, 90); //distance from xstep,ystep to CB
+            float beta = distance(xstep, ystep, 61, 10, 25, 90) / distance(100, 100, 61, 10, 25, 90); //distance from xstep,ystep to AC
+            float gamma = distance(xstep, ystep, 100, 100, 61, 10) / distance(25, 90, 100, 100, 61, 10); //distance from xstep,ystep to BA
+            if(alpha < 0.0 || beta < 0.0 || gamma < 0.0){
+                image << 0 << " " << 0 << " " << 0 << endl;
+                continue;
+            }
+            cout << alpha << " " << beta << " " << gamma << endl;
+            
+            
+            int R = 255 * alpha;
+            int G = 255 * beta;
+            int B = 255 * gamma;
+           
+           
+            R = (R < 0)? 0 : (R > 255)? 255 : R;
+            G = (G < 0)? 0 : (G > 255)? 255 : G;
+            B = (B < 0)? 0 : (B > 255)? 255 : B;
+
+            image << R << " " << G << " " << B << endl;
+        }
+    }
+
+}
 
 int main(int argc, char ** argv){
     if(argc < 4){
@@ -62,7 +154,7 @@ int main(int argc, char ** argv){
     image << "#" << endl;
     image << argv[2] << " " << argv[3] << endl;
     image << "255" << endl;
-    baryinterp(image, stoi(argv[2]), stoi(argv[3]));
+    fullcolour(image, stoi(argv[2]), stoi(argv[3]));
     image.close();
     return 0;
 }
