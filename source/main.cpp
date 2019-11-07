2f20526ee1fc64608d9a22601c7c23fe501ec7dc
Govind Venkatesh <venkateshgovind1@gmail.com>
2019-11-07 23:22:14 +0000

    interpolation not working

--- source/main.cpp
+++ source/main.cpp
@@ -191,15 +191,50 @@ void readPPM(ifstream & in){
 
             float b1 = (0.160268 * alpha) + (0.083611 * beta) + (0.230169 * gamma);
             float b2 = (0.290086 * alpha) + (0.159907 * beta) + (0.222781 * gamma);
+            
+            float s = x * b1;
+            float t = y * b2;
 
-            int texelx = x * b1;
-            int texely = y * b2;
+            int i = s;
+            int j = s;
 
-            int R = pixels[texelx][texely*3];
-            int G = pixels[texelx][texely*3+1];
-            int B = pixels[texelx][texely*3+2];
+            float sParm = s - i;
+            float tParm = t - j;
 
-            image << R << " " << G << " " << B << " " ;
+            //ij
+            int R0 = pixels[i][j*3];
+            int G0 = pixels[i][j*3+1];
+            int B0 = pixels[i][j*3+2];
+            //ij+1
+            int R1 = pixels[i][(j+1)*3];
+            int G1 = pixels[i][(j+1)*3+1];
+            int B1 = pixels[i][(j+1)*3+2];
+            //i+1j
+            int R2 = pixels[i+1][j*3];
+            int G2 = pixels[i+1][j*3+1];
+            int B2 = pixels[i+1][j*3+2];
+            //i+1j+1
+            int R3 = pixels[i+1][(j+1)*3];
+            int G3 = pixels[i+1][(j+1)*3+1];
+            int B3 = pixels[i+1][(j+1)*3+2];
+
+            //colours on edges
+            int cR0, cG0, cB0, cR1, cG1, cB1;
+            cR0 = R0 + tParm * (R1 - R0);
+            cG0 = G0 + tParm * (G1 - G0);
+            cB0 = B0 + tParm * (B1 - B0);
+
+            cR1 = R2 + tParm * (R3 - R2);
+            cG1 = R2 + tParm * (G3 - G2);
+            cB1 = R2 + tParm * (B3 - B2);
+
+            //calculate interpolated texel
+            int colourR, colourG, colourB;
+            colourR = cR1 + sParm * (cR1 - cR0);
+            colourG = cG1 + sParm * (cG1 - cG0);
+            colourB = cB1 + sParm * (cB1 - cB0);
+            
+            image << colourR << " " << colourG << " " << colourB << " " ;
         }
         image << endl;
     }
